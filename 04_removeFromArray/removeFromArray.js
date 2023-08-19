3119371ee67cb77896440a8e71918562b68c1608
Ganiyu Olowookere <wedesign4u11@gmail.com>
2023-08-19 10:08:00 +0100

    Code passed for every test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function ([...newaray], ...strin) {
+  let parray = [];
 
+  for (let i = 0; i < newaray.length; i++) {
+    if (strin.indexOf(newaray[i]) === -1) {
+      parray.push(newaray[i]);
+    }
+  }
+
+  return parray;
 };
 
 // Do not edit below this line
