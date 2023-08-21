c6a5cb949d0aa3977ae328bd1ff74ce1c998e359
Ganiyu Olowookere <wedesign4u11@gmail.com>
2023-08-21 22:09:37 +0100

    Code passed all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function (n, secondNum) {
+  let m = n - 1;
+  let x = 0;
+  let newSecNum = secondNum - 1;
 
+  if (n < 0) {
+    x = "ERROR";
+  }
+
+  if (typeof n !== "number" || typeof secondNum !== "number") {
+    x = "ERROR";
+  }
+
+  if (n > secondNum) {
+    while (newSecNum < n) {
+      newSecNum++;
+      x += newSecNum;
+    }
+  }
+
+  if (typeof n == "number" && typeof secondNum == "number" && n > 0) {
+    while (m < secondNum) {
+      m++;
+      x += m;
+    }
+  } else {
+    x = "ERROR";
+  }
+
+  return x;
 };
 
 // Do not edit below this line
