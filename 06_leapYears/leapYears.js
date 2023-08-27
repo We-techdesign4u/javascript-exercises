7a793550429132b5bb69afe341d89f080ffda904
Ganiyu Olowookere <wedesign4u11@gmail.com>
2023-08-27 10:36:30 +0100

    Code passed all test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (Year) {
+  if (Year % 4 == 0 && Year % 100 != 0) {
+    return true;
+  } else if (Year % 4 == 0 && Year % 100 == 0) {
+    if (Year % 400 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
