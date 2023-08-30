05c82b6b41697c442646aa7c8b4a8c252ac9ba75
Ganiyu Olowookere <wedesign4u11@gmail.com>
2023-08-30 15:51:27 +0100

    Code pass all test for TempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  //x °F ≘ (x − 32) × 5/9 °C
+  let result = (fahrenheit - 32) * (5 / 9);
+  return Math.round(result * 10) / 10;
+  // return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  //x °C ≘ (x × 9/5 + 32) °F
+  let secresult = celsius * (9 / 5) + 32;
+  return Math.round(secresult * 10) / 10;
+  // return secresult;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
