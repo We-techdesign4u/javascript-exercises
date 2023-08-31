39613c19d94ae589479fffb0b187d096c6eec07d
Ganiyu Olowookere <wedesign4u11@gmail.com>
2023-08-31 11:36:05 +0100

    Code pass all test for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (addNum1, addNum2) {
+  return addNum1 + addNum2;
 };
 
-const subtract = function() {
-	
+const subtract = function (subNum1, subNum2) {
+  return subNum1 - subNum2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sumarr = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sumarr += arr[i];
+  }
+  return sumarr;
 };
 
-const multiply = function() {
-
+const multiply = function (mult) {
+  let multarr = 1;
+  for (let index = 0; index < mult.length; index++) {
+    multarr = multarr * mult[index];
+  }
+  return multarr;
 };
 
-const power = function() {
-	
+const power = function (pow1, pow2) {
+  return Math.pow(pow1, pow2);
 };
 
-const factorial = function() {
-	
+const factorial = function (agu) {
+  let mul = 1;
+  for (i = 1; i <= agu; i++) {
+    mul *= i;
+  }
+  return mul;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
