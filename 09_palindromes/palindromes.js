6d31d8396ea00939bcf67e1572cf58cab8568866
Ganiyu Olowookere <wedesign4u11@gmail.com>
2023-09-01 10:52:08 +0100

    Code pass all test for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,8 @@
 const palindromes = function (word) {
-  var punctuation =
-    /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
-  var spaceRE = /\s+/g;
-  var wordnopunc = word.replace(punctuation, "").replace(spaceRE, "");
-  let text = "";
-
-  const myArray = wordnopunc.split("");
-  const reverse = myArray.reverse();
-  for (let i = 0; i < myArray.length; i++) {
-    text += myArray[i];
-  }
+  var wordnopunc = word.replace(/[^a-zA-Z0-9]/g, "");
+  const text = wordnopunc.split("").reverse().join("");
   return wordnopunc.toLowerCase() === text.toLowerCase();
 };
-//   return text;
 
 // Do not edit below this line
 module.exports = palindromes;
